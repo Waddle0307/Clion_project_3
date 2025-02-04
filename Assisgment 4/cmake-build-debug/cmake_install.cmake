b05e77d6680b48b6de72c8d693a61dcf80cb96e5
pjgonzalez <paulgonzalez1231@gmail.com>
2025-02-04 11:47:39 -0800

    Learning C assigment 4 (upadte 04/02/2025)

--- Assisgment 4/cmake-build-debug/cmake_install.cmake
+++ Assisgment 4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/pjgonzalez/CLionProjects/untitled1
+# Install script for directory: /Users/pjgonzalez/CLionProjects/Assisgment 4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
